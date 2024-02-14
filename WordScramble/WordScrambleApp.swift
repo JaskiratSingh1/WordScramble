96eaf6efe3a243e84f40169f651124d9a81fcc32
Jaskirat Singh <jaskirat1999@gmail.com>
2024-02-13 20:26:06 -0800

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Jaskirat Singh on 2024-02-13.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
