4dcaec8ffae17d775249ee4bbbafc71b26134d63
Jaskirat Singh <jaskirat1999@gmail.com>
2024-02-14 22:50:18 -0800

    Added animations

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -17,11 +17,15 @@ struct ContentView: View {
             List {
                 Section {
                     TextField("Enter your word", text: $newWord)
+                        .textInputAutocapitalization(.never)
                 }
                 
                 Section {
                     ForEach(usedWords, id: \.self) {word in
+                        HStack {
+                            Image(systemName: "\(word.count).circle")
                             Text(word)
+                        }
                     }
                 }
             }
@@ -35,9 +39,10 @@ struct ContentView: View {
         
         guard answer.count > 0 else { return }
         
-        
-        usedWords.insert(answer, at: 0)
-        newWord = ""
+        withAnimation {
+            usedWords.insert(answer, at: 0)
+            newWord = ""
+        }
     }
 }
 
